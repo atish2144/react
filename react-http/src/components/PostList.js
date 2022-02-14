84b5f45bc7da6c2bb8432991d5c4f1b8d6502879
atish2144 <jagtapatish555@gmail.com>
2022-02-14 11:50:13 +0530

    kj

---
+++ react-http/src/components/PostList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class PostList extends Component {
+  render() {
+    return <div>List Of Posts</div>;
+  }
+}
+
+export default PostList;
