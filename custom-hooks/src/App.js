5f3caead125f048790fb23deb5917d6b7f3b9810
atish2144 <jagtapatish555@gmail.com>
2022-02-25 12:53:17 +0530

    change

---
+++ custom-hooks/src/App.js
@@ -0,0 +1,18 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import DocTitleOne from "./components/DocTitleOne";
+// import DocTitleTwo from "./components/DocTitleTwo";
+import USeForm from "./hooks/USeForm";
+function App() {
+  return (
+    <div className="App">
+      {/* <DocTitleOne></DocTitleOne>
+      <DocTitleTwo></DocTitleTwo> */}
+      {/* <Counter1></Counter1>
+      <Counter2></Counter2> */}
+      <USeForm></USeForm>
+    </div>
+  );
+}
+
+export default App;
