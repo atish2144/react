9f7d8bc14cd6c7c9aac0715eed44fbcce7707558
atish2144 <jagtapatish555@gmail.com>
2022-03-07 12:05:36 +0530

    gd

--- tic-tac-toe/src/SquareComponent.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-
-function SquareComponent(props) {
-  const classes = props.className ? `${props.className}square` : "square";
-  return <span className={classes}>x</span>;
-}
-
-export default SquareComponent;
